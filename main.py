e218534be67685a677a3e92271d78e4a0028eb10
TtIiMmOoNnAa <104669378+TtIiMmOoNnAa@users.noreply.github.com>
2023-11-18 17:41:45 +0500

    Second commit. Добавил цикл в функции для вывода Hi 5 раз.

--- main.py
+++ main.py
@@ -6,7 +6,8 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    for _ in  range(5):
+        print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
